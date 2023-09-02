b87a36515897b0116282bef21ce67632a1fd1c36
mlevinmail <mlevinmail@gmail.com>
2023-09-01 21:12:14 -0600

    rr

---
+++ src/OpenAIChat.js
@@ -0,0 +1,57 @@
+export class OpenAIChat {
+    constructor(apiKey, initialSystemMessage = 'You are a helpful assistant during a voice chat.') {
+      this.apiKey = apiKey;
+      this.messages = [];
+  
+      if (initialSystemMessage) {
+        this.addSystemMessage(initialSystemMessage);
+      }
+    }
+  
+    addSystemMessage(content) {
+      this.messages.push({
+        role: 'system',
+        content: content
+      });
+    }
+  
+    addUserMessage(content) {
+      this.messages.push({
+        role: 'user',
+        content: content
+      });
+    }
+  
+    async getResponse() {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.apiKey}`
+        },
+        body: JSON.stringify({
+          model: 'gpt-4',
+          messages: this.messages
+        })
+      });
+  
+      const data = await response.json();
+  
+      if (data && data.choices && data.choices.length > 0) {
+        const message = data.choices[0].message;
+        this.messages.push({
+          role: message.role,
+          content: message.content
+        });
+        return message.content;
+      } else {
+        throw new Error('Failed to get a valid response from the OpenAI API.');
+      }
+    }
+  
+    resetChat() {
+      this.messages = [];
+    }
+  }
+
+  
---
+++ src/transcriber.js
@@ -0,0 +1,124 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { OpenAIChat } from './OpenAIChat';
+import './transcriber.css';  // A new CSS file for styling
+
+const Transcriber = () => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [conversationTrail, setConversationTrail] = useState('');
+    const [selectedLang, setSelectedLang] = useState('en-US');
+    const mediaRecorder = useRef(null);
+    const audioChunks = useRef([]);
+    const [stream, setStream] = useState(null);
+    const chat = useRef(new OpenAIChat('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'));
+
+    useEffect(() => {
+        const textarea = document.getElementById("conversationTrail");
+        textarea.scrollTop = textarea.scrollHeight;
+    }, [conversationTrail]);
+
+    const toggleRecording = () => {
+        if (!isRecording) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+        setIsRecording(!isRecording);
+    };
+
+    const startRecording = () => {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(s => {
+            setStream(s);
+            mediaRecorder.current = new MediaRecorder(s);
+            mediaRecorder.current.ondataavailable = event => {
+                audioChunks.current.push(event.data);
+            };
+
+            mediaRecorder.current.onstop = async () => {
+                const audioBlob = new Blob(audioChunks.current);
+                await sendToWhisper(audioBlob);
+                audioChunks.current = [];
+            };
+
+            mediaRecorder.current.start();
+        });
+    };
+
+    const stopRecording = () => {
+        mediaRecorder.current.stop();
+        if (stream) {
+            stream.getTracks().forEach(track => track.stop());
+        }
+    };
+
+    const sendToWhisper = async (audioBlob) => {
+        const formData = new FormData();
+        formData.append("file", audioBlob, "recording.mp3");
+        formData.append("model", "whisper-1");
+
+        try {
+            const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer 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'
+                },
+                body: formData
+            });
+
+            const data = await response.json();
+            chat.current.addUserMessage(data.text);
+            setConversationTrail(prev => `${prev}\nUser: ${data.text}`);
+            const openAiResponse = await chat.current.getResponse();
+            setConversationTrail(prev => `${prev}\nAssistant: ${openAiResponse}`);
+
+            // Read out the assistant's response
+            const speech = new SpeechSynthesisUtterance(openAiResponse);
+            // speech speed
+            speech.rate = 1.2;
+            speech.lang = selectedLang;
+            window.speechSynthesis.speak(speech);
+
+        } catch (error) {
+            console.error("Error transcribing audio:", error);
+        }
+    };
+
+    function scrollToBottom() {
+        const chatArea = document.getElementById('chatArea');
+        chatArea.scrollTop = chatArea.scrollHeight;
+    }
+
+
+    const resetConversation = () => {
+        setConversationTrail('');
+        chat.current.resetChat();
+
+        // Stop the speech synthesis if it's reading aloud
+        window.speechSynthesis.cancel();
+    };
+
+    return (
+        <div className="transcriber-container">
+            <select value={selectedLang} onChange={(e) => setSelectedLang(e.target.value)}>
+                <option value="en-US">English (US)</option>
+                <option value="ru-RU">Russian</option>
+                <option value="uk-UA">Ukrainian</option>
+                <option value="fr-FR">French</option>
+                <option value="de-DE">German</option>
+                <option value="zh-HK">Chinese (Hong Kong)</option>
+            </select>
+
+            <textarea id="conversationTrail" value={conversationTrail} readOnly rows={20} placeholder="Conversation trail..." />
+            <div className="button-group">
+                <button className={isRecording ? 'btn stop' : 'btn start'} onClick={toggleRecording}>
+                    {isRecording ? 'Stop' : 'Start'} Recording
+                </button>
+                <button className="btn reset" onClick={resetConversation}>
+                    Reset Conversation
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Transcriber;
+
